79451b0ada963c3930db81782d88d59acc5a2a94
hyewoncc <hyewoncc@gmail.com>
2021-04-20 23:29:12 +0900

    generate spring boot project

---
+++ src/main/java/com/modooary/ModooaryApplication.java
@@ -0,0 +1,13 @@
+package com.modooary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModooaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModooaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modooary/ModooaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modooary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModooaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
